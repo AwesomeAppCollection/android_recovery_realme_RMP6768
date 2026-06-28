11ab60e1f5a2bdacd9c7de45b60d890f3684b854
Blurple Calculator <awesomegamingofficial7@gmail.com>
2026-06-28 13:59:13 +0530

    Update twrp_RMP6768.mk

--- twrp_RMP6768.mk
+++ twrp_RMP6768.mk
@@ -42,4 +42,3 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31 \
     ro.bootimage.build.date.utc=0 \
-    ro.build.date.utc=0
